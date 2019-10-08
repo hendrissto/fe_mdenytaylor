33b8dd42c2c8570830e259e2f1c9f05e815acad1
Fahmi Agung Tajul Abidin <fata.22032001@gmail.com>
2019-10-09 02:14:23 +0700

    [Improve] Improve UI, Add Is Real user, change format date.

--- src/components/cards/IconCard.js
+++ src/components/cards/IconCard.js
@@ -5,21 +5,40 @@ import IntlMessages from "../../helpers/IntlMessages";
 const IconCard = ({ className = "mb-4", icon, title, value }) => {
   return (
     <div className={`icon-row-item ${className}`}>
-      <Card className="bg-primary text-white" style={{"background":"linear-gradient(90deg, rgba(24,67,143,1) 32%, rgba(31,102,175,1) 61%)"}}>
+      <Card
+        className="bg-primary text-white"
+        style={{
+          background:
+            "linear-gradient(90deg, rgba(24,67,143,1) 32%, rgba(31,102,175,1) 61%);",
+          borderRadius: 6
+        }}
+      >
         <CardBody className="text-center">
           {icon && (
             <Row>
-              <Col xs="2"> <h1 style={{ fontSize: '-webkit-xxx-large ' }}><i className={icon} /></h1> </Col>
+              <Col xs="2">
+                {" "}
+                <h1 style={{ fontSize: "-webkit-xxx-large " }}>
+                  <i className={icon} />
+                </h1>{" "}
+              </Col>
             </Row>
           )}
           <Row>
-            <Col xs="6"><h6 className="text-left" style={{wordBreak: 'keep-all'}}>
-              <span style={{
-                marginBottom: 30
-              }}><IntlMessages id={title} /></span>
-            </h6>
+            <Col xs="6">
+              <h6 className="text-left" style={{ wordBreak: "keep-all" }}>
+                <span
+                  style={{
+                    marginBottom: 30
+                  }}
+                >
+                  <IntlMessages id={title} />
+                </span>
+              </h6>
             </Col>
-            <Col><h1 className="text-right" >{value}</h1></Col>
+            <Col>
+              <h1 className="text-right">{value}</h1>
+            </Col>
           </Row>
         </CardBody>
       </Card>
