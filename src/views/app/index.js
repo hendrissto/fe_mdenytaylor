711dcf7b553301df225e9173b44eec34520a0d1a
arifakhri <arifakhri97@gmail.com>
2020-07-06 15:11:07 +0700

    fix conflict stash

--- src/views/app/index.js
+++ src/views/app/index.js
@@ -6,7 +6,7 @@ import { AclService } from "../../services/auth/AclService";
 
 import AppLayout from "../../layout/AppLayout";
 
-import Notifications from "../../containers/applications/Notifications";
+// // import Notifications from "../../containers/applications/Notifications";
 import dashboard from "./dashboard";
 import CODReceiptNumber from "./cod-receipt-number";
 import Tenant from "./tenants";
@@ -70,7 +70,7 @@ class App extends Component {
           <GuardedRoute path={`${match.url}/users`} component={Users} meta={{permissions: ['admin.user_admin.view']}} />
           <GuardedRoute path={`${match.url}/roles`} component={UserRoles} meta={{permissions: ['admin.role_admin.view']}} />
           <GuardedRoute path={`${match.url}/permissions`} component={ListPermission} meta={{permissions: ['admin.permission_admin.view']}} />
-          <Route path={`${match.url}/notifications`} component={Notifications} />
+          {/* {/* <Route path={`${match.url}/notifications`} component={Notifications} /> */} */}
       </GuardProvider>
           {/* route dummy */}
           {/* <GuardedRoute path={`${match.url}/dashboards`} component={dashboards} meta={{permissions: ['dashboard.general.view']}} />
